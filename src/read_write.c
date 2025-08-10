c716a03ec08c4f110e6c89cc6ba631e196b393dd
Pargev Malkhasyan <pamalkha@c2r9s1.42yerevan.am>
2025-08-10 17:13:01 +0400

    "test"

--- src/read_write.c
+++ src/read_write.c
@@ -6,20 +6,23 @@
 /*   By: pamalkha <pamalkha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/13 14:31:00 by pargev            #+#    #+#             */
-/*   Updated: 2025/08/10 15:38:25 by pamalkha         ###   ########.fr       */
+/*   Updated: 2025/08/10 16:04:57 by pamalkha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
 //actually "pwd" (as well as "cd") should take one and only one arg, but... will fix later
-int	run_builtin(char **cmds, t_minishell *data)
+int	run_builtin(char *command, char **cmds, t_minishell *data)
 {
-	if (!ft_strncmp(cmds[0], "cd", 2))
+	(void)command;
+	if (!ft_strncmp(cmds[0], "cd", 3))
 		return (cd(cmds, data));
-	else if (!ft_strncmp(cmds[0], "pwd\0", 4))
+	else if (!ft_strncmp(cmds[0], "pwd", 4))
 		return (pwd(cmds));
-	else if (!ft_strncmp(cmds[0], "exit\0", 5))
+	else if (!ft_strncmp(cmds[0], "exit", 5))
+		return (exit_cmd(cmds, data));
+	else if (!ft_strncmp(cmds[0], "echo", 5))
 		return (exit_cmd(cmds, data));
 	return (0);
 }
@@ -34,7 +37,7 @@ void	analize_command(char *command, t_minishell *data)
 	{
 		add_history(command);
 		cmds = parse_words(command);
-		if (!run_builtin(cmds, data))
+		if (!run_builtin(command, cmds, data))
 			exec(cmds);
 		free_arr(cmds, NULL);
 	}
